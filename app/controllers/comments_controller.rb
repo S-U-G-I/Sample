ffe0dd68dbad838ad5be303dd8740683d6d78384
Tatsu <tatsutatsu3355@gmail.com>
2019-09-17 01:10:39 +0000

    Mypage

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,13 @@ def create
   redirect_to root_path
 end
 
+def destroy
+   @comment=Comment.find_by(id: params[:tweet_id])
+   @comment.destroy
+   flash[:notice] = "削除しました"
+   redirect_to root_path
+end
+
 private
   def comment_params
     params.require(:comment).permit(:user_id, :tweet_id, :text)
